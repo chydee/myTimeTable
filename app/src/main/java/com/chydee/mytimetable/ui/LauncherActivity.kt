a00ce348dd9507995f34f8aceccb3abf2f7d0c44
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-01-10 17:17:08 +0100

    Completed project setup and Created launcher screen

--- app/src/main/java/com/chydee/mytimetable/ui/LauncherActivity.kt
+++ app/src/main/java/com/chydee/mytimetable/ui/LauncherActivity.kt
@@ -1,7 +1,9 @@
 package com.chydee.mytimetable.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.chydee.mytimetable.MainActivity
@@ -35,4 +37,24 @@ class LauncherActivity : AppCompatActivity() {
         })
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUIAndNavigation(this)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun hideSystemUIAndNavigation(activity: Activity) {
+        val decorView: View = activity.window.decorView
+        decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
 }
