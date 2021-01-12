fb2102e0405a4221a9e696069bdaf5b149cb415e
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-01-12 05:46:11 +0100

    Generate Tests for Database with Dagger-hilt

--- app/src/main/java/com/chydee/mytimetable/di/AppModule.kt
+++ app/src/main/java/com/chydee/mytimetable/di/AppModule.kt
@@ -1,28 +1,9 @@
 package com.chydee.mytimetable.di
 
-import android.content.Context
-import androidx.room.Room
-import com.chydee.mytimetable.data.MyTimetableDB
-import com.chydee.mytimetable.utils.Constants
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
-
-    @Singleton
-    @Provides
-    fun provideRunningDatabase(
-        @ApplicationContext app: Context
-    ) = Room.databaseBuilder(
-        app,
-        MyTimetableDB::class.java,
-        Constants.TEST_DB_NAME
-    ).fallbackToDestructiveMigration()
-        .build()
-}
+object AppModule
---
+++ app/src/main/java/com/chydee/mytimetable/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.chydee.mytimetable.di
+
+import android.content.Context
+import androidx.room.Room
+import com.chydee.mytimetable.data.MyTimetableDB
+import com.chydee.mytimetable.data.dao.PeriodDao
+import com.chydee.mytimetable.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        MyTimetableDB::class.java,
+        Constants.TEST_DB_NAME
+    ).fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    fun providePeriodDao(database: MyTimetableDB): PeriodDao = database.periodDao
+}
