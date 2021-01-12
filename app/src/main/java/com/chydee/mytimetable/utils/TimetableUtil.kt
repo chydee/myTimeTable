5140c840633bb09a331739b64648bc28b638c6fb
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-01-12 10:11:17 +0100

    Test for timetable inpute validation

--- app/src/main/java/com/chydee/mytimetable/utils/TimetableUtil.kt
+++ app/src/main/java/com/chydee/mytimetable/utils/TimetableUtil.kt
@@ -2,6 +2,15 @@ package com.chydee.mytimetable.utils
 
 object TimetableUtil {
 
+
+    private val timeTableNames = arrayListOf(
+        "Computer Science Department",
+        "Life Science",
+        "Physics",
+        "Maths & Stats",
+        "Geology"
+    )
+
     /**
      * The input is not valid if...
      * ... @param timetableFor is empty
@@ -13,12 +22,33 @@ object TimetableUtil {
         courseCode: String,
         courseTitle: String,
         courseLabel: Int,
-        courseTutor: String,
+        courseTutor: String = "Unknown",
         place: String,
         dayOfTheWeek: String,
         startTime: String,
         endTime: String
     ): Boolean {
+        if (timetableFor.isNotEmpty() && timetableFor in timeTableNames) {
+            return false
+        }
+        if (timetableFor.isEmpty()) {
+            return false
+        }
+        if (id <= -1 || courseCode.isEmpty()) {
+            return false
+        }
+        if (courseTitle.isEmpty() || courseTitle.contains("-?\\d+(\\.\\d+)?".toRegex())) {
+            return false
+        }
+        if (courseLabel <= 0) {
+            return false
+        }
+        if (courseTutor.isEmpty() || dayOfTheWeek.isEmpty() || place.isEmpty()) {
+            return false
+        }
+        if (startTime.isEmpty() || endTime.isEmpty()) {
+            return false
+        }
         return true
     }
 }
--- app/src/test/java/com/chydee/mytimetable/utils/TimetableUtilTest.kt
+++ app/src/test/java/com/chydee/mytimetable/utils/TimetableUtilTest.kt
@@ -6,10 +6,92 @@ import org.junit.Test
 class TimetableUtilTest {
 
     @Test
+    fun `when timeTable for already exists return false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "Geology", 0, "CSC111", "Intro To Computing", 5, "Test tutor", "1000PK",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `when timeTableFor is not empty and does not already exists return true`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "Language", 0, "CSC111", "Intro To Computing", 5, "Test tutor", "1000PK",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `when every field is empty or invalid return false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "", -1, "", "", -1, "", "",
+            "", "", ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
     fun `empty course code returns false`() {
         val result = TimetableUtil.validateTimetableInput(
-            "school", 0, "", "5", 5, "test tutor", "1000PK",
+            "school", 0, "", "Intro To Computing", 5, "Test tutor", "1000PK",
             "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty course title returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "", 6, "Test tutor", "1000PK",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `invalid course label returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "Intro To Computing", -1, "Test tutor", "1000PK",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+
+    @Test
+    fun `empty course tutor returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "Intro To Computing", -1, "", "1000PK",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty place  returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "Intro To Computing", -1, "Test Tutor", "",
+            "Mon", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty or invalid day of the week returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "Intro To Computing", 5, "Test tutor", "1000PK",
+            "", "11:00", "13:00"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty start and end time returns false`() {
+        val result = TimetableUtil.validateTimetableInput(
+            "school", 0, "CSC111", "Intro To Computing", -1, "Test tutor", "1000PK",
+            "Mon", "", ""
         )
         assertThat(result).isFalse()
     }
